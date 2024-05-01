7cea9bff79930c0a647e201e29eae6610bbdd36b
kushalb-dev <kushalb.dev@gmail.com>
2024-05-01 22:37:45 +0530

    Changing the collection name

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,7 +16,7 @@ const db = getFirestore(firebase);
 export const createProduct = async (req, res, next) => {
   try {
     const data = req.body;
-    await addDoc(collection(db, 'products'), data);
+    await addDoc(collection(db, 'UserPost'), data);
     res.status(200).send('product created successfully');
   } catch (error) {
     res.status(400).send(error.message);
@@ -55,7 +55,7 @@ export const getProducts = async (req, res, next) => {
 export const getProduct = async (req, res, next) => {
   try {
     const id = req.params.id;
-    const product = doc(db, 'products', id);
+    const product = doc(db, 'UserPost', id);
     const data = await getDoc(product);
     if (data.exists()) {
       res.status(200).send(data.data());
@@ -71,7 +71,7 @@ export const updateProduct = async (req, res, next) => {
   try {
     const id = req.params.id;
     const data = req.body;
-    const product = doc(db, 'products', id);
+    const product = doc(db, 'UserPost', id);
     await updateDoc(product, data);
     res.status(200).send('product updated successfully');
   } catch (error) {
@@ -82,7 +82,7 @@ export const updateProduct = async (req, res, next) => {
 export const deleteProduct = async (req, res, next) => {
   try {
     const id = req.params.id;
-    const deletedProduct = await getDoc(doc(db, 'products', id));
+    const deletedProduct = await getDoc(doc(db, 'UserPost', id));
     await deleteDoc(doc(db, 'products', id));
     res.status(200).send(`product deleted successfully: ${deletedProduct.id}`);
   } catch (error) {
